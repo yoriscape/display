cd360b2f2796a158eb2be6007c39dde7400f3f14
Padmanabhan Komanduru <quic_pkomandu@quicinc.com>
2022-07-18 13:44:27 +0530

    sdm: clear cached brightness at the end of TUI
    Clear cached brightness during TUI Transition End event
    when display is going to power off. This avoids brightness
    being zero on next power on.
    
    Change-Id: I3a0c925be157f2836f9de924e82a81ad96cbe018
    CRs-Fixed: 3223582

--- sdm/libs/core/display_builtin.cpp
+++ sdm/libs/core/display_builtin.cpp
@@ -1092,6 +1092,11 @@ DisplayError DisplayBuiltIn::SetDisplayState(DisplayState state, bool teardown,
     return error;
   }
 
+  if (secure_event_ == kTUITransitionEnd && state == kStateOff) {
+    SetPanelBrightness(cached_brightness_);
+    pending_brightness_ = false;
+  }
+
   if (hw_panel_info_.mode != panel_mode) {
     UpdateDisplayModeParams();
   }
