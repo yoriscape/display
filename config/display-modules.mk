2df25d8376d8638370d3c630716fc411c45f16aa
Gurkaran Singh <quic_gurksing@quicinc.com>
2022-11-08 08:22:22 -0800

    composer3: Move conditional compilation block
    Move conditional compilation block to under headless macro to
    avoid any conflicts when compiling in headless mode.
    
    CRs-Fixed: 3331672
    Change-Id: I6675a187b3e2e8cf8d2a5691df6542fa6100185e

--- config/display-modules.mk
+++ config/display-modules.mk
@@ -3,11 +3,6 @@ DISPLAY_MODULES_HARDWARE:= android.hardware.graphics.mapper@4.0-impl-qti-display
                         vendor.qti.hardware.display.allocator-service
 
 TARGET_IS_HEADLESS := false
-ifeq ($(TARGET_USES_COMPOSER3),true)
-DISPLAY_MODULES_HARDWARE += vendor.qti.hardware.display.composer3-service
-else
-DISPLAY_MODULES_HARDWARE += vendor.qti.hardware.display.composer-service
-endif
 
 ifeq ($(TARGET_USES_QMAA),true)
     ifneq ($(TARGET_USES_QMAA_OVERRIDE_DISPLAY),true)
@@ -18,6 +13,12 @@ endif
 
 #Packages that should not be installed in QMAA are enabled here
 ifneq ($(TARGET_IS_HEADLESS),true)
+    ifeq ($(TARGET_USES_COMPOSER3),true)
+    DISPLAY_MODULES_HARDWARE += vendor.qti.hardware.display.composer3-service
+    else
+    DISPLAY_MODULES_HARDWARE += vendor.qti.hardware.display.composer-service
+    endif
+
     DISPLAY_MODULES_HARDWARE += libsdmcore \
                             libsdmdal \
                             libdrmutils \
