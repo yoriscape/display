5d38d5f8d35d2683c927ac7fb61c513d2c8c4cc3
Ramkumar Radhakrishnan <quic_rradhakr@quicinc.com>
2023-03-21 14:39:37 -0700

    hwc: update correct BSD license information
    Change-Id: I721c66a5852621915afaf9be76d0d99838509755

--- composer/DisplayConfigAIDL.cpp
+++ composer/DisplayConfigAIDL.cpp
@@ -1,8 +1,6 @@
 /*
 * Copyright (c) 2021 The Linux Foundation. All rights reserved.
 *
-* Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
-*
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions are
 * met:
@@ -28,6 +26,14 @@
 * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
+
+/*
+ * Changes from Qualcomm Innovation Center are provided under the following license:
+ *
+ * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
+
 #include "DisplayConfigAIDL.h"
 
 using sdm::Locker;
