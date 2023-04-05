45547fcb178d3163e81dace2c0b7bf31f1554b87
Alex Sarraf <quic_asarraf@quicinc.com>
2023-04-04 17:12:51 -0700

    sdm: Update license and copyright markings
    Update license and copyright markings.
    
    CRs-Fixed: 3455929
    Change-Id: Iec0d1f102d4bde15bf538a93c5cf50783a44f2d9

--- sdm/include/private/strategy_interface.h
+++ sdm/include/private/strategy_interface.h
@@ -1,8 +1,6 @@
 /*
 * Copyright (c) 2014 - 2019, 2021, The Linux Foundation. All rights reserved.
 *
-* Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
-*
 * Redistribution and use in source and binary forms, with or without modification, are permitted
 * provided that the following conditions are met:
 *    * Redistributions of source code must retain the above copyright notice, this list of
@@ -23,6 +21,12 @@
 * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
+
+/* Changes from Qualcomm Innovation Center are provided under the following license:
+ *
+ * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
 
 #ifndef __STRATEGY_INTERFACE_H__
 #define __STRATEGY_INTERFACE_H__
--- sdm/libs/core/display_virtual.cpp
+++ sdm/libs/core/display_virtual.cpp
@@ -1,8 +1,6 @@
 /*
 * Copyright (c) 2014 - 2021, The Linux Foundation. All rights reserved.
 *
-* Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
-*
 * Redistribution and use in source and binary forms, with or without modification, are permitted
 * provided that the following conditions are met:
 *    * Redistributions of source code must retain the above copyright notice, this list of
