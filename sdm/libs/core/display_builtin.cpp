600441de36e33b6b9e1771f8c2da6e24c83c260f
Mitika Dodiya <quic_mdodiya@quicinc.com>
2023-05-22 18:09:16 +0530

    sdm: disable pu for one frame on demura config switch
    fix issue of distorted patterns when demura configs are
    switched during video playback in portrait mode. Disable
    pu for one frame during demura multiconfig switch.
    
    Change-Id: I1e78ab8cd1323bdd650a6d677fa1f5c7bc24ebc3

--- sdm/libs/core/display_builtin.cpp
+++ sdm/libs/core/display_builtin.cpp
@@ -3194,6 +3194,9 @@ DisplayError DisplayBuiltIn::SetDemuraConfig(int demura_idx) {
   DLOGV("Demura config updated to config index %d", demura_idx);
   event_handler_->Refresh();
 
+  // disable partial update for one frame
+  DisablePartialUpdateOneFrame();
+
   return kErrorNone;
 }
 
