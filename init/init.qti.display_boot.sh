75608fda612cdd55d9bf1b1715df674ed8e1e306
Vikas batchu <quic_vikabatc@quicinc.com>
2023-09-29 01:33:46 +0530

    init: set properties for cliffs target
    Change-Id: I3a240ad5928a3a8237a2c59372886abb828b04c8

--- init/init.qti.display_boot.sh
+++ init/init.qti.display_boot.sh
@@ -56,6 +56,24 @@ case "$target" in
         setprop vendor.display.timed_render_enable 1
         setprop vendor.gralloc.hw_supports_ubwcp 0
         ;;
+      614|632)
+        # SOC ID for Palawan is 614
+        # SOC ID for Lamma is 632
+        setprop vendor.display.enable_fb_scaling 0
+        setprop vendor.gralloc.use_dma_buf_heaps 1
+        setprop vendor.display.target.version 5
+        setprop vendor.display.enable_posted_start_dyn 2
+        setprop vendor.display.enable_allow_idle_fallback 1
+        setprop vendor.display.enable_perf_hint_large_comp_cycle 1
+        setprop vendor.display.enable_rotator_ui 1
+        setprop vendor.display.enable_spec_fence 1
+        setprop vendor.display.thermal.version 1
+        setprop vendor.display.enable_rc_support 1
+        setprop vendor.display.enable_latch_media_content 1
+        setprop vendor.display.enable_inline_writeback 1
+        setprop vendor.display.timed_render_enable 1
+        setprop vendor.gralloc.hw_supports_ubwcp 0
+        ;;
     esac
     ;;
     "kalama")
