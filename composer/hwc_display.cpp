LineageOS/android_hardware_qcom_display

composer: Trigger refresh at the end of animation
Change-Id: I655521044a03789250f4583a0a2c836e79408a31

composer/hwc_display.cpp
@@ -1217,6 +1217,16 @@ HWC2::Error HWCDisplay::GetPerFrameMetadataKeys(uint32_t *out_num_keys,
   return HWC2::Error::None;
 }
 
+HWC2::Error HWCDisplay::SetDisplayAnimating(bool animating) {
+  // Trigger refresh, when animation ends.
+  if (!animating) {
+    callbacks_->Refresh(id_);
+  }
+
+  animating_ = animating;
+  return HWC2::Error::None;
+}
+
 HWC2::Error HWCDisplay::GetActiveConfig(hwc2_config_t *out_config) {
   if (out_config == nullptr) {
     return HWC2::Error::BadDisplay;

composer/hwc_display.h
@@ -406,10 +406,7 @@ class HWCDisplay : public DisplayEventHandler {
                                          float* out_min_luminance);
   virtual HWC2::Error GetPerFrameMetadataKeys(uint32_t *out_num_keys,
                                               PerFrameMetadataKey *out_keys);
-  virtual HWC2::Error SetDisplayAnimating(bool animating) {
-    animating_ = animating;
-    return HWC2::Error::None;
-  }
+  virtual HWC2::Error SetDisplayAnimating(bool animating);
   virtual bool IsDisplayCommandMode();
   virtual HWC2::Error SetQSyncMode(QSyncMode qsync_mode) {
     return HWC2::Error::Unsupported;

yoriscape/display

composer/hwc_display.cpp
@@ -1221,6 +1221,16 @@ HWC3::Error HWCDisplay::GetPerFrameMetadataKeys(uint32_t *out_num_keys,
   return HWC3::Error::None;
 }
 
+HWC3::Error HWCDisplay::SetDisplayAnimating(bool animating) {
+  // Trigger refresh, when animation ends.
+  if (!animating) {
+    callbacks_->Refresh(id_);
+  }
+
+  animating_ = animating;
+  return HWC3::Error::None;
+}
+
 HWC3::Error HWCDisplay::GetActiveConfig(Config *out_config) {
   if (out_config == nullptr) {
     return HWC3::Error::BadDisplay;

composer/hwc_display.h
@@ -376,10 +376,7 @@ class HWCDisplay : public DisplayEventHandler {
                                                   AlphaInterpretation *alpha);
   virtual HWC3::Error GetPerFrameMetadataKeys(uint32_t *out_num_keys,
                                               PerFrameMetadataKey *out_keys);
-  virtual HWC3::Error SetDisplayAnimating(bool animating) {
-    animating_ = animating;
-    return HWC3::Error::None;
-  }
+  virtual HWC3::Error SetDisplayAnimating(bool animating);
   virtual bool IsDisplayCommandMode();
   virtual HWC3::Error SetQSyncMode(QSyncMode qsync_mode) { return HWC3::Error::Unsupported; }
   virtual DisplayError ControlIdlePowerCollapse(bool enable, bool synchronous) {