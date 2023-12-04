LineageOS/android_hardware_qcom_display

composer: Force GPU comp for wfd if color transform required
If color transform is not identity, force GPU composition
for wfd to make appearence consistent with primary

Change-Id: If7d22ef485c9266e3678a4bed7b3407417bf87f2

composer/hwc_display_virtual_dpu.cpp
@@ -200,6 +200,10 @@ HWC2::Error HWCDisplayVirtualDPU::PreValidateDisplay(bool *exit_validate) {
     }
   }
 
+  if (force_gpu_comp_ && !layer_stack_.flags.secure_present) {
+    MarkLayersForClientComposition();
+  }
+
   *exit_validate = false;
 
   return HWC2::Error::None;
@@ -273,6 +277,12 @@ HWC2::Error HWCDisplayVirtualDPU::SetPanelLuminanceAttributes(float min_lum, flo
   if (err != kErrorNone) {
     return HWC2::Error::BadParameter;
   }
+  return HWC2::Error::None;
+}
+
+HWC2::Error HWCDisplayVirtualDPU::SetColorTransform(const float *matrix,
+                                                    android_color_transform_t hint) {
+  force_gpu_comp_ = (hint != HAL_COLOR_TRANSFORM_IDENTITY) ? true : false;
   return HWC2::Error::None;
 }
 

yoriscape/display

composer/hwc_display_virtual_dpu.cpp
@@ -167,6 +167,10 @@ HWC3::Error HWCDisplayVirtualDPU::PreValidateDisplay(bool *exit_validate) {
     }
   }
 
+  if (force_gpu_comp_ && !layer_stack_.flags.secure_present) {
+    MarkLayersForClientComposition();
+  }
+
   *exit_validate = false;
 
   return HWC3::Error::None;
@@ -235,6 +239,12 @@ HWC3::Error HWCDisplayVirtualDPU::SetPanelLuminanceAttributes(float min_lum, flo
   if (err != kErrorNone) {
     return HWC3::Error::BadParameter;
   }
+  return HWC3::Error::None;
+}
+
+HWC3::Error HWCDisplayVirtualDPU::SetColorTransform(const float *matrix,
+                                                    android_color_transform_t hint) {
+  force_gpu_comp_ = (hint != HAL_COLOR_TRANSFORM_IDENTITY) ? true : false;
   return HWC3::Error::None;
 }
 