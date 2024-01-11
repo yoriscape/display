d3b0d3ffe2a1514ec4c51f84ea9e386be21f8c39
BOREDDY MAHIDHAR <quic_bmahidha@quicinc.com>
2024-01-11 11:01:29 +0530

    gralloc: Add support for Transfer_SMPTE_ST2084 in color_metadata
    Change-Id: I89326957f0f1596ab9116e74211c3de5c0359e74

--- gralloc/gr_buf_mgr.cpp
+++ gralloc/gr_buf_mgr.cpp
@@ -18,7 +18,7 @@
  */
 /*
  * Changes from Qualcomm Innovation Center are provided under the following license:
- * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause-Clear
 */
 
@@ -127,6 +127,9 @@ static Error dataspaceToColorMetadata(Dataspace dataspace, ColorMetaData *color_
     case (uint32_t)Dataspace::TRANSFER_HLG:
       out.transfer = Transfer_HLG;
       break;
+    case (uint32_t)Dataspace::TRANSFER_ST2084:
+      out.transfer = Transfer_SMPTE_ST2084;
+      break;
     default:
       return Error::UNSUPPORTED;
       /*
@@ -138,7 +141,6 @@ static Error dataspaceToColorMetadata(Dataspace dataspace, ColorMetaData *color_
       Transfer_sYCC
       Transfer_BT2020_2_1
       Transfer_BT2020_2_2
-      Transfer_SMPTE_ST2084
       Transfer_ST_428
       */
   }
