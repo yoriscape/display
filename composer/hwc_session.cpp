LineageOS/android_hardware_qcom_display

composer: handle active display state properly
Change-Id: I9de296f9a89153f3339aa46e3d11fd03a6c6538f

composer/hwc_session.cpp
@@ -73,6 +73,7 @@ static const uint32_t kBrightnessScaleMax = 100;
 static const uint32_t kSvBlScaleMax = 65535;
 Locker HWCSession::vm_release_locker_[HWCCallbacks::kNumDisplays];
 std::bitset<HWCCallbacks::kNumDisplays> HWCSession::clients_waiting_for_vm_release_;
+std::set<hwc2_display_t> HWCSession::active_displays_;
 
 
 // Map the known color modes to dataspace.
@@ -1213,6 +1214,12 @@ int32_t HWCSession::SetPowerMode(hwc2_display_t display, int32_t int_mode) {
                           active_builtin_disp_id < HWCCallbacks::kNumDisplays);
 
     }
+
+    if (mode == HWC2::PowerMode::Off || mode == HWC2::PowerMode::DozeSuspend) {
+      active_displays_.erase(display);
+    } else {
+      active_displays_.insert(display);
+    }
     auto error = CallDisplayFunction(display, &HWCDisplay::SetPowerMode, mode,
                                      false /* teardown */);
     if (INT32(error) != HWC2_ERROR_NONE) {
@@ -1354,7 +1361,6 @@ HWC2::Error HWCSession::CreateVirtualDisplayObj(uint32_t width, uint32_t height,
       *out_display_id = client_id;
       map_info.disp_type = kVirtual;
       map_info.sdm_id = display_id;
-      map_active_displays_.insert(std::make_pair(client_id, map_info.disp_type));
       break;
     }
   }
@@ -2732,7 +2738,6 @@ int HWCSession::CreatePrimaryDisplay() {
           DLOGW("Failed to load HWCColorManager.");
         }
 
-        map_active_displays_.insert(std::make_pair(client_id, info.display_type));
       } else {
         DLOGE("Primary display creation has failed! status = %d", status);
         return status;
@@ -2828,7 +2833,6 @@ int HWCSession::HandleBuiltInDisplays() {
         map_info.sdm_id = info.display_id;
         CreateDummyDisplay(client_id);
 
-        map_active_displays_.insert(std::make_pair(client_id, info.display_type));
       }
 
       DLOGI("Hotplugging builtin display, sdm id = %d, client id = %d", info.display_id,
@@ -3000,7 +3004,6 @@ int HWCSession::HandleConnectedDisplays(HWDisplaysInfo *hw_displays_info, bool d
         DLOGI("Created pluggable display successfully: sdm id = %d, client id = %d",
               info.display_id, UINT32(client_id));
 
-        map_active_displays_.insert(std::make_pair(client_id, map_info.disp_type));
         CreateDummyDisplay(client_id);
       }
 
@@ -3145,7 +3148,7 @@ void HWCSession::DestroyPluggableDisplay(DisplayMapInfo *map_info) {
       }
     }
 
-    map_active_displays_.erase(client_id);
+    active_displays_.erase(client_id);
     display_ready_.reset(UINT32(client_id));
     pending_power_mode_[client_id] = false;
     hwc_display = nullptr;
@@ -3186,7 +3189,7 @@ void HWCSession::DestroyNonPluggableDisplay(DisplayMapInfo *map_info) {
         hwc_display_dummy = nullptr;
       }
     }
-    map_active_displays_.erase(client_id);
+    active_displays_.erase(client_id);
 
     pending_power_mode_[client_id] = false;
     hwc_display = nullptr;
@@ -3378,8 +3381,15 @@ void HWCSession::HandlePendingPowerMode(hwc2_display_t disp_id,
     if (display != active_builtin_disp_id) {
       Locker::ScopeLock lock_d(locker_[display]);
       if (pending_power_mode_[display] && hwc_display_[display]) {
+        HWC2::PowerMode pending_mode = hwc_display_[display]->GetPendingPowerMode();
+
+        if (pending_mode == HWC2::PowerMode::Off || pending_mode == HWC2::PowerMode::DozeSuspend) {
+          active_displays_.erase(display);
+        } else {
+          active_displays_.insert(display);
+        }
         HWC2::Error error =
-          hwc_display_[display]->SetPowerMode(hwc_display_[display]->GetPendingPowerMode(), false);
+          hwc_display_[display]->SetPowerMode(pending_mode, false);
         if (HWC2::Error::None == error) {
           pending_power_mode_[display] = false;
           hwc_display_[display]->ClearPendingPowerMode();
@@ -4195,7 +4205,7 @@ HWC2::Error HWCSession::CommitOrPrepare(hwc2_display_t display, bool validate_on
   {
     SEQUENCE_ENTRY_SCOPE_LOCK(locker_[display]);
     hwc_display_[display]->ProcessActiveConfigChange();
-    hwc_display_[display]->IsMultiDisplay((map_active_displays_.size() > 1) ? true : false);
+    hwc_display_[display]->IsMultiDisplay((active_displays_.size() > 1) ? true : false);
     status = hwc_display_[display]->CommitOrPrepare(validate_only, out_retire_fence, out_num_types,
                                                     out_num_requests, needs_commit);
   }

composer/hwc_session.h
@@ -349,6 +349,7 @@ class HWCSession : hwc2_device_t, HWCUEventListener, public qClient::BnQClient,
   static int commit_error_[HWCCallbacks::kNumDisplays];
   static Locker vm_release_locker_[HWCCallbacks::kNumDisplays];
   static std::bitset<HWCCallbacks::kNumDisplays> clients_waiting_for_vm_release_;
+  static std::set<hwc2_display_t> active_displays_;
 
  private:
   class CWB {
@@ -646,7 +647,6 @@ class HWCSession : hwc2_device_t, HWCUEventListener, public qClient::BnQClient,
   std::shared_ptr<IPCIntf> ipc_intf_ = nullptr;
   bool primary_pending_ = true;
   Locker primary_display_lock_;
-  std::map <hwc2_display_t, sdm::DisplayType> map_active_displays_;
 };
 }  // namespace sdm
 

yoriscape/display

composer/hwc_session.cpp
@@ -82,6 +82,7 @@ static const uint32_t kBrightnessScaleMax = 100;
 static const uint32_t kSvBlScaleMax = 65535;
 Locker HWCSession::vm_release_locker_[HWCCallbacks::kNumDisplays];
 std::bitset<HWCCallbacks::kNumDisplays> HWCSession::clients_waiting_for_vm_release_;
+std::set<Display> HWCSession::active_displays_;
 
 // Map the known color modes to dataspace.
 int32_t GetDataspaceFromColorMode(ColorMode mode) {
@@ -1229,11 +1230,16 @@ HWC3::Error HWCSession::SetPowerMode(Display display, int32_t int_mode) {
     return HWC3::Error::None;
   }
 
-    auto error =
-        CallDisplayFunction(display, &HWCDisplay::SetPowerMode, mode, false /* teardown */);
-    if (error != HWC3::Error::None) {
-      return error;
-    }
+  if (mode == PowerMode::OFF || mode == PowerMode::DOZE_SUSPEND) {
+    active_displays_.erase(display);
+  } else {
+    active_displays_.insert(display);
+  }
+
+  auto error = CallDisplayFunction(display, &HWCDisplay::SetPowerMode, mode, false /* teardown */);
+  if (error != HWC3::Error::None) {
+    return error;
+  }
   // Reset idle pc ref count on suspend, as we enable idle pc during suspend.
   if (mode == PowerMode::OFF) {
     idle_pc_ref_cnt_ = 0;
@@ -3364,6 +3370,7 @@ void HWCSession::DestroyPluggableDisplayLocked(DisplayMapInfo *map_info) {
   }
 
   map_active_displays_.erase(client_id);
+  active_displays_.erase(client_id);
   display_ready_.reset(UINT32(client_id));
   pending_power_mode_[client_id] = false;
   hwc_display = nullptr;
@@ -3400,6 +3407,7 @@ void HWCSession::DestroyNonPluggableDisplayLocked(DisplayMapInfo *map_info) {
   }
 
   map_active_displays_.erase(client_id);
+  active_displays_.erase(client_id);
 
   pending_power_mode_[client_id] = false;
   hwc_display = nullptr;
@@ -3637,8 +3645,10 @@ void HWCSession::HandlePendingPowerMode(Display disp_id, const shared_ptr<Fence>
 
     if (pending_mode == PowerMode::OFF || pending_mode == PowerMode::DOZE_SUSPEND) {
       map_active_displays_.erase(display);
+      active_displays_.erase(display);
     } else {
       map_active_displays_.insert(std::make_pair(disp_map_info->client_id, disp_map_info));
+      active_displays_.insert(display);
     }
     HWC3::Error error = hwc_display_[display]->SetPowerMode(pending_mode, false);
     if (HWC3::Error::None == error) {
@@ -4597,7 +4607,7 @@ HWC3::Error HWCSession::CommitOrPrepare(Display display, bool validate_only,
   {
     SEQUENCE_ENTRY_SCOPE_LOCK(locker_[display]);
     hwc_display_[display]->ProcessActiveConfigChange();
-    hwc_display_[display]->IsMultiDisplay((map_active_displays_.size() > 1) ? true : false);
+    hwc_display_[display]->IsMultiDisplay((active_displays_.size() > 1) ? true : false);
     status = hwc_display_[display]->CommitOrPrepare(validate_only, out_retire_fence, out_num_types,
                                                     out_num_requests, needs_commit);
   }

composer/hwc_session.h
@@ -341,6 +341,7 @@ class HWCSession : public HWCUEvent,
   static int commit_error_[HWCCallbacks::kNumDisplays];
   static Locker vm_release_locker_[HWCCallbacks::kNumDisplays];
   static std::bitset<HWCCallbacks::kNumDisplays> clients_waiting_for_vm_release_;
+  static std::set<Display> active_displays_;
 
  private:
   class CWB {